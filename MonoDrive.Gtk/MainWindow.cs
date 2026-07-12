03b90ee7a715a033bd5e64d29ae336c0966d627d
Bruno B. de Souza <knuxbbs@gmail.com>
2026-07-12 12:44:49 -0300

    replaced entry with button and dialog

--- MonoDrive.Gtk/MainWindow.cs
+++ MonoDrive.Gtk/MainWindow.cs
@@ -11,9 +11,10 @@ namespace MonoDrive.Gtk
         private global::Gtk.ApplicationWindow _window;
         private global::Gtk.Label _userLabel;
         private global::Gtk.Label _folderLabel;
-        private global::Gtk.Entry _folderEntry;
+        private global::Gtk.Button _chooseFolderButton;
         private global::Gtk.Button _syncButton;
         private global::Gtk.ProgressBar _progressBar;
+        private string _selectedFolderPath;
 
         public MainWindow(IMainWindowPresenter mainWindowPresenter)
         {
@@ -45,10 +46,9 @@ namespace MonoDrive.Gtk
             _folderLabel.SetHalign(global::Gtk.Align.Start);
             layout.Append(_folderLabel);
 
-            _folderEntry = global::Gtk.Entry.New();
-            _folderEntry.SetPlaceholderText("Caminho da pasta local");
-            _folderEntry.OnChanged += FolderEntry_Changed;
-            layout.Append(_folderEntry);
+            _chooseFolderButton = global::Gtk.Button.NewWithLabel("Escolher pasta…");
+            _chooseFolderButton.OnClicked += ChooseFolderButton_Clicked;
+            layout.Append(_chooseFolderButton);
 
             var loginButton = global::Gtk.Button.NewWithLabel("Login");
             loginButton.OnClicked += LoginButton_Clicked;
@@ -72,18 +72,45 @@ namespace MonoDrive.Gtk
         private async Task LoadContentAsync()
         {
             _userLabel.SetText(await _mainWindowPresenter.GetUserEmail());
-            
+
             var localRootDirectory = await _mainWindowPresenter.GetLocalRootDirectory();
 
             if (!string.IsNullOrEmpty(localRootDirectory))
             {
-                _folderLabel.SetText(localRootDirectory);
-                _folderEntry.SetText(localRootDirectory);
-                _syncButton.Sensitive = Directory.Exists(localRootDirectory);
+                SetSelectedFolder(localRootDirectory);
             }
         }
 
         public void Show() => _window?.Present();
+
+        private async void ChooseFolderButton_Clicked(global::Gtk.Button sender, EventArgs args)
+        {
+            try
+            {
+                var dialog = global::Gtk.FileDialog.New();
+                dialog.SetTitle("Selecione a pasta de sincronização");
+
+                if (!string.IsNullOrEmpty(_selectedFolderPath) && Directory.Exists(_selectedFolderPath))
+                    dialog.SetInitialFolder(Gio.FileHelper.NewForPath(_selectedFolderPath));
+
+                var folder = await dialog.SelectFolderAsync(_window);
+                var path = folder?.GetPath();
+
+                if (!string.IsNullOrEmpty(path))
+                    SetSelectedFolder(path);
+            }
+            catch (Exception ex)
+            {
+                _progressBar.Text = $"Erro ao selecionar pasta: {ex.Message}";
+            }
+        }
+
+        private void SetSelectedFolder(string path)
+        {
+            _selectedFolderPath = path;
+            _folderLabel.SetText(path);
+            _syncButton.Sensitive = Directory.Exists(path);
+        }
 
         private async void LoginButton_Clicked(global::Gtk.Button sender, EventArgs args)
         {
@@ -91,38 +118,29 @@ namespace MonoDrive.Gtk
             {
                 _userLabel.SetText(await _mainWindowPresenter.GetUserEmail());
             }
-            catch (Exception e)
+            catch (Exception ex)
             {
-                throw; // TODO handle exception
+                _progressBar.Text = $"Erro ao fazer login: {ex.Message}";
             }
-        }
-
-        private void FolderEntry_Changed(global::Gtk.Editable sender, EventArgs args)
-        {
-            var selectedPath = _folderEntry.GetText();
-            _folderLabel.SetText(selectedPath);
-            _syncButton.Sensitive = Directory.Exists(selectedPath);
         }
 
         private async void SyncButton_Clicked(global::Gtk.Button sender, EventArgs args)
         {
             try
             {
-                var selectedPath = _folderEntry.GetText();
-
-                if (!Directory.Exists(selectedPath))
+                if (!Directory.Exists(_selectedFolderPath))
                 {
                     _progressBar.Text = "Diretório inválido.";
                     return;
                 }
 
                 _progressBar.Text = "Sincronizando...";
-                await _mainWindowPresenter.Sync(selectedPath);
+                await _mainWindowPresenter.Sync(_selectedFolderPath);
                 _progressBar.Text = "Sincronização concluída.";
             }
-            catch (Exception e)
+            catch (Exception ex)
             {
-                throw; // TODO handle exception
+                _progressBar.Text = $"Erro na sincronização: {ex.Message}";
             }
         }
     }
